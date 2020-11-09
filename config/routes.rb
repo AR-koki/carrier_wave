ba0124042e999859e54821c43554293c6fb9c004
AR-koki <teruuuuun72@gmail.com>
2020-11-09 12:41:06 +0000

    [Add]user.index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:create, :index, :show, :edit, :update, :destroy, :new]
-  resources :users, only: [:show, :edit, :update, :index, :create]
+  resources :users, only: [:index,:show, :edit, :update, :index, :create]
   resources :categorise, only: [:index, :show]
 end
