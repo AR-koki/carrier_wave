126cb44c41192f1c00464f3901cbce1e42ce6f2e
AR-koki <teruuuuun72@gmail.com>
2020-11-09 12:04:05 +0000

    [Add]user_association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name:'Relationships', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   mount_uploader :avater, AvaterUploader
 end
