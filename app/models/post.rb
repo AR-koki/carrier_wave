85602546b007a91ccccd6aa419a1bb1c4ae20995
AR-koki <teruuuuun72@gmail.com>
2020-11-10 12:43:20 +0000

    [Add]Rspec_test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 	belongs_to :user, optional: true
 	belongs_to :category,optional: true
 	mount_uploaders :images, ImageUploader
+
+	validates :title, presence: true
 end
